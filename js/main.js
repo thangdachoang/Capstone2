0036acaa3b6b863a02dea062581654d6ce743999
thangdachoang <thang.dachoang@gmail.com>
2022-08-20 19:34:50 +0700

    done component

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+// Modal giỏ hàng
+
+
 var spService = new ProductsService();
 function getProductList() {
   spService
@@ -16,19 +19,19 @@ getProductList();
 function hieuThiSanPham(mangSP) {
   var content = "";
   mangSP.map(function (newProducts, index) {
-    var i = 0;
+    
     content += `
             <div class="card" style="width: 18rem;">
                 <img src=${newProducts.img} class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">${newProducts.name}</h5>
                     <p class="card-text">${newProducts.price}</p>
-                    <button id="btnThemVaoGioHang" class="btn btn-success">Thêm vào giỏ hàng</button>
+                    <button  onclick="themSPGioHang('${newProducts.id}')" class="btn btn-success">Thêm vào giỏ hàng</button>
                 </div>
             </div>
           
         `;
-        i++;
+        
     // console.log(newProducts);
   });
 
@@ -58,5 +61,49 @@ function timKiemSP() {
 var cart= [];
 
 
+function themSPGioHang (id){
+  spService.getProduct(id).then(function(result){
+    var spGioHang = result.data;
+    var cartItem ={product : {spGioHang} ,quality : 1};
+    checkGioHang(cartItem);
+    showGioHang(cart);
+  })
+  .catch(function(error){
+  });
+}
 
+function checkGioHang(cartItem){
+  if(cart ==""){
+    cart.push(cartItem);
+  }else{
+    var flag = true;
+    cart.map(function(Item, index){
+      if(cartItem.product.spGioHang.id == Item.product.spGioHang.id){
+        Item.quality++;
+        flag = false
+      }
+    });
+    if(flag){
+      cart.push(cartItem);
+    }
+  }
+}
+
+function showGioHang(mangSP){
+  var content = "";
+  mangSP.map(function(Products, index){
+    content += `
+      <tr>
+      <td>${Products.product.spGioHang.name}</td>
+      <td>${Products.product.spGioHang.price}</td>
+      <td>${Products.quality}</td>
+      <td>
+      <button class="btn btn-warning">Remove</button>
+      </td>
+      </tr>
+    `
+  })
+
+  document.getElementById("tbodySP").innerHTML=content;
+}
 
